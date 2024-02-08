ad77e328360c7c2406465f6d809606e81b363fbb
anil <anilkumar.kammalapalli@tarento.com>
2024-02-08 13:28:50 +0530

    update property names

--- notification/notification-sdk/src/main/java/org/sunbird/notification/beans/Constants.java
+++ notification/notification-sdk/src/main/java/org/sunbird/notification/beans/Constants.java
@@ -2,11 +2,11 @@ package org.sunbird.notification.beans;
 
 public class Constants {
 
-  public static final String EMAIL_SERVER_FROM = "sunbird_mail_server_from_email";
-  public static final String EMAIL_SERVER_HOST = "sunbird_mail_server_host";
-  public static final String EMAIL_SERVER_PASSWORD = "sunbird_mail_server_password";
-  public static final String EMAIL_SERVER_PORT = "sunbird_mail_server_port";
-  public static final String EMAIL_SERVER_USERNAME = "sunbird_mail_server_username";
+  public static final String EMAIL_SERVER_FROM = "mail_server_from_email";
+  public static final String EMAIL_SERVER_HOST = "mail_server_host";
+  public static final String EMAIL_SERVER_PASSWORD = "mail_server_password";
+  public static final String EMAIL_SERVER_PORT = "mail_server_port";
+  public static final String EMAIL_SERVER_USERNAME = "mail_server_username";
   public static final String FROM_EMAIL = "fromEmail";
   public static final String EMAIL_ORG_IMG_URL = "orgImageUrl";
 }
